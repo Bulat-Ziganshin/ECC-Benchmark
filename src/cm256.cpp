27fa3c1017b1725717e14bf6ce58791f8fe2922b
Bulat Ziganshin <bulat.ziganshin@gmail.com>
2022-07-17 05:50:31 +0300

    CM256: first real benchmark
    First results on i7-8665U:
    
    378 usec, 542 MB/s
    4 usec, 48450 MB/s

--- src/cm256.cpp
+++ src/cm256.cpp
@@ -7,50 +7,76 @@
 // Benchmark CM256 library, return false if anything failed
 bool bench_cm256(ECC_bench_params params)
 {
+    uint64_t encode_time = 0, decode_time = 0;  // Total encode/decode times
+
     if (cm256_init()) {
         printf("cm256_init failed\n");
         return false;
     }
 
-    // Allocate and fill the original file data
+    // Allocate the original file data and recovery data
     uint8_t* originalFileData = new uint8_t[params.OriginalFileBytes()];
-    memset(originalFileData, 1, params.OriginalFileBytes());
-
-    // Pointers to data
-    cm256_block blocks[256];
-    for (int i = 0; i < params.OriginalCount; ++i)
-    {
-        blocks[i].Block = originalFileData + i * params.BlockBytes;
-    }
-
-    // Recovery data
     uint8_t* recoveryBlocks = new uint8_t[params.RecoveryCount * params.BlockBytes];
 
-    // Generate recovery data
-    if (cm256_encode(params, blocks, recoveryBlocks))
+    // Repeat benchmark multiple times to improve its accuracy
+    const int trials = 1000;
+    for (int trial = 0; trial < trials; ++trial)
     {
-        printf("cm256_encode failed\n");
-        return false;
+        // Fill the original file data
+        for (size_t i = 0; i < params.OriginalFileBytes(); ++i) {
+            originalFileData[i] = (uint8_t)((i*123456791) >> 13);
+        }
+
+        // Pointers to data
+        cm256_block blocks[256];
+        for (int i = 0; i < params.OriginalCount; ++i)
+        {
+            blocks[i].Block = originalFileData + i * params.BlockBytes;
+        }
+
+        uint64_t t0 = siamese::GetTimeUsec();
+        // Generate recovery data
+        if (cm256_encode(params, blocks, recoveryBlocks))
+        {
+            printf("cm256_encode failed\n");
+            return false;
+        }
+        uint64_t t1 = siamese::GetTimeUsec();
+        encode_time += t1 - t0;
+
+        // Initialize the indices
+        for (int i = 0; i < params.OriginalCount; ++i)
+        {
+            blocks[i].Index = cm256_get_original_block_index(params, i);
+        }
+
+        //// Simulate loss of data, subsituting a recovery block in its place ////
+        blocks[0].Block = recoveryBlocks; // First recovery block
+        blocks[0].Index = cm256_get_recovery_block_index(params, 0); // First recovery block index
+        //// Simulate loss of data, subsituting a recovery block in its place ////
+
+        t0 = siamese::GetTimeUsec();
+        if (cm256_decode(params, blocks))
+        {
+            printf("cm256_decode failed\n");
+            return false;
+        }
+        t1 = siamese::GetTimeUsec();
+        decode_time += t1 - t0;
+
+        // blocks[0].Index will now be 0.
     }
 
-    // Initialize the indices
-    for (int i = 0; i < params.OriginalCount; ++i)
     {
-        blocks[i].Index = cm256_get_original_block_index(params, i);
+        const double opusec = double(encode_time) / trials;
+        const double mbps = params.OriginalFileBytes() / opusec;
+        printf("%.0lf usec, %.0lf MB/s\n", opusec, mbps);
     }
-
-    //// Simulate loss of data, subsituting a recovery block in its place ////
-    blocks[0].Block = recoveryBlocks; // First recovery block
-    blocks[0].Index = cm256_get_recovery_block_index(params, 0); // First recovery block index
-    //// Simulate loss of data, subsituting a recovery block in its place ////
-
-    if (cm256_decode(params, blocks))
     {
-        printf("cm256_decode failed\n");
-        return false;
+        const double opusec = double(decode_time) / trials;
+        const double mbps = params.OriginalFileBytes() / opusec;
+        printf("%.0lf usec, %.0lf MB/s\n", opusec, mbps);
     }
-
-    // blocks[0].Index will now be 0.
 
     delete[] originalFileData;
     delete[] recoveryBlocks;
