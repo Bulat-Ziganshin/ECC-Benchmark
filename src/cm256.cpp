3e8de31b9fa98b2cba3a629757d777bbba354baf
Bulat Ziganshin <bulat.ziganshin@gmail.com>
2022-07-18 00:11:55 +0300

    CM256: use parity block #1 for decoding
    Decoding single lost data block using parity block #0 is much faster than other decoding scenarios, since in CM256 parity block #0 computed as XOR of all input blocks.
    
    Also split encode and decode functions in order to make code even more readable.

--- src/cm256.cpp
+++ src/cm256.cpp
@@ -6,83 +6,110 @@
 #include "common.h"
 
 
-// Perform encoding and decoding operations, return false if anything failed
-bool cm256_run_trials(
+// Perform single encoding operation, return false if it failed
+bool cm256_benchmark_encode(
     ECC_bench_params params,
     uint8_t* originalFileData,
     uint8_t* recoveryBlocks,
-    uint64_t& encode_time,
+    uint64_t& encode_time)
+{
+    // Pointers to data
+    cm256_block blocks[256];
+    for (int i = 0; i < params.OriginalCount; ++i)
+    {
+        blocks[i].Block = originalFileData + i * params.BlockBytes;
+    }
+
+    uint64_t t0 = siamese::GetTimeUsec();
+    // Generate recovery data
+    if (cm256_encode(params, blocks, recoveryBlocks))
+    {
+        printf("  cm256_encode failed\n");
+        return false;
+    }
+    uint64_t t1 = siamese::GetTimeUsec();
+    encode_time += t1 - t0;
+
+    return true;
+}
+
+
+// Perform single decoding operation, return false if it failed
+bool cm256_benchmark_decode(
+    ECC_bench_params params,
+    uint8_t* originalFileData,
+    uint8_t* recoveryBlocks,
     uint64_t& decode_time)
 {
-    // Repeat benchmark multiple times to improve its accuracy
-    for (int trial = 0; trial < params.Trials; ++trial)
+    // Pointers to data
+    cm256_block blocks[256];
+
+    // Initialize the indices
+    for (int i = 0; i < params.OriginalCount; ++i)
     {
-        // Pointers to data
-        cm256_block blocks[256];
-        for (int i = 0; i < params.OriginalCount; ++i)
-        {
-            blocks[i].Block = originalFileData + i * params.BlockBytes;
-        }
-
-        uint64_t t0 = siamese::GetTimeUsec();
-        // Generate recovery data
-        if (cm256_encode(params, blocks, recoveryBlocks))
-        {
-            printf("  cm256_encode failed\n");
-            return false;
-        }
-        uint64_t t1 = siamese::GetTimeUsec();
-        encode_time += t1 - t0;
-
-        // Initialize the indices
-        for (int i = 0; i < params.OriginalCount; ++i)
-        {
-            blocks[i].Index = cm256_get_original_block_index(params, i);
-        }
-
-        //// Simulate loss of data, subsituting a recovery block in its place ////
-        blocks[0].Block = recoveryBlocks; // First recovery block
-        blocks[0].Index = cm256_get_recovery_block_index(params, 0); // First recovery block index
-        //// Simulate loss of data, subsituting a recovery block in its place ////
-
-        t0 = siamese::GetTimeUsec();
-        if (cm256_decode(params, blocks))
-        {
-            printf("  cm256_decode failed\n");
-            return false;
-        }
-        t1 = siamese::GetTimeUsec();
-        decode_time += t1 - t0;
-
-        // blocks[0].Index will now be 0.
+        blocks[i].Block = originalFileData + i * params.BlockBytes;
+        blocks[i].Index = cm256_get_original_block_index(params, i);
     }
+
+    //// Simulate loss of data, subsituting a recovery block in its place ////
+    int lostBlock = params.RecoveryCount==1? 0 : 1;  // Since recovery block #0 recovers much faster using just XORs
+    blocks[0].Block = recoveryBlocks + lostBlock * params.BlockBytes; // A recovery block
+    blocks[0].Index = cm256_get_recovery_block_index(params, lostBlock); // A recovery block index
+    //// Simulate loss of data, subsituting a recovery block in its place ////
+
+    uint64_t t0 = siamese::GetTimeUsec();
+    if (cm256_decode(params, blocks))
+    {
+        printf("  cm256_decode failed\n");
+        return false;
+    }
+    uint64_t t1 = siamese::GetTimeUsec();
+    decode_time += t1 - t0;
+
+    // blocks[0].Index will now be = lostBlock
+    // and blocks[0].Block overwritten with recovered data.
 
     return true;
 }
 
 
 // Print the benchmark results, return false if anything failed
-bool cm256_print_results(
+bool cm256_benchmark_print_results(
     ECC_bench_params params,
     uint8_t* originalFileData,
     uint8_t* recoveryBlocks)
 {
     uint64_t encode_time = 0, decode_time = 0;  // Total encode/decode times
 
+    if (cm256_init()) {
+        printf("cm256_init failed\n");
+        return false;
+    }
+
+    // Print CPU SIMD extensions used to accelerate CM256 in this run
+    // (depends on compilation options such as -mavx2 and actual CPU)
     printf("CM256 (%s, %d-bit):\n",
-#ifdef GF256_TRY_AVX2
+#ifndef GF256_TARGET_MOBILE
+#  ifdef GF256_TRY_AVX2
         CpuHasAVX2? "avx2":
-#endif
+#  endif
         CpuHasSSSE3? "ssse3":
+#endif
 #if defined(GF256_TRY_NEON)
         CpuHasNeon64? "neon64":
         CpuHasNeon? "neon":
 #endif
         "", sizeof(size_t)*8);
 
-    if (! cm256_run_trials(params, originalFileData, recoveryBlocks, encode_time, decode_time))
+    // Repeat benchmark multiple times to improve its accuracy
+    for (int trial = 0; trial < params.Trials; ++trial)
     {
-        return false;
+        if (! cm256_benchmark_encode(params, originalFileData, recoveryBlocks, encode_time)) {
+            return false;
+        }
+        if (! cm256_benchmark_decode(params, originalFileData, recoveryBlocks, decode_time)) {
+            return false;
+        }
     }
 
     {
@@ -104,11 +131,6 @@ bool cm256_print_results(
 // This function allocates and initializes memory buffers used in encode/decode
 bool cm256_benchmark_main(ECC_bench_params params)
 {
-    if (cm256_init()) {
-        printf("cm256_init failed\n");
-        return false;
-    }
-
     // Allocate the original file data and recovery data
     auto original = std::make_unique<uint8_t[]>(params.OriginalFileBytes());
     auto recovery = std::make_unique<uint8_t[]>(params.RecoveryCount * params.BlockBytes);
@@ -120,5 +142,5 @@ bool cm256_benchmark_main(ECC_bench_params params)
         originalFileData[i] = (uint8_t)((i*123456791) >> 13);
     }
 
-    return cm256_print_results(params, originalFileData, recoveryBlocks);
+    return cm256_benchmark_print_results(params, originalFileData, recoveryBlocks);
 }
